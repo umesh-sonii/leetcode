782f88df2d0b40fcc05803292945d807e05fc13d
umesh-sonii <itsmeumeshsoni@gmail.com>
2025-09-11 22:02:27 +0530

    Time: 2 ms (57.16%) | Memory: 10 MB (11.52%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()) return false;
+        unordered_map<char, int> mpp;
 
-        vector<int> hash(26, 0);
-
-        for(int i=0; i<s.size(); i++) {
-            hash[s[i] - 'a']++;
-            hash[t[i] - 'a']--;
+        for(int i=0; i<s.length(); i++) {
+            mpp[s[i]]++;
         }
-
-        for(int i=0; i<26; i++) {
-            if(hash[i] != 0) return false;
+        for(int j=0; j<t.length(); j++) {
+            mpp[t[j]]--;
+        }
+        for(auto it: mpp){
+            if(it.second != 0) return false;
         }
 
         return true;
