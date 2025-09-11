a95b22fc16c4ca366bc44eea27b9ae2fc66ecf35
umesh-sonii <itsmeumeshsoni@gmail.com>
2025-09-11 21:56:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (74.13%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+
+        vector<int> hash(26, 0);
+
+        for(int i=0; i<s.size(); i++) {
+            hash[s[i] - 'a']++;
+            hash[t[i] - 'a']--;
+        }
+
+        for(int i=0; i<26; i++) {
+            if(hash[i] != 0) return false;
+        }
+
+        return true;
+    }
+};
