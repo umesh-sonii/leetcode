5a93812c22ff86f042e2c7caca157a828e646039
umesh-sonii <itsmeumeshsoni@gmail.com>
2025-10-11 11:17:06 +0530

    Time: 9 ms (60.37%) | Memory: 20.4 MB (5.13%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        vector<int> freq(26,0);
-        queue<int> que;
+        vector<int> freq(26, 0);
+        queue<int> q;
 
         for(int i=0; i<s.length(); i++) {
             char ch = s[i];
             freq[ch - 'a']++;
-            que.push(i);
 
-            while(!que.empty() > 0 && freq[s[que.front()] - 'a'] > 1) que.pop();
+            q.push(i);
+
+            while(!q.empty() && freq[s[q.front()] - 'a'] > 1) q.pop();
         }
 
-        return que.empty() ? -1 : que.front();
+        return q.empty() ? -1 : q.front();
     }
 };
