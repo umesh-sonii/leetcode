e1d46c156dc241e0e72f20cc6c6dc80b6dffca1f
umesh-sonii <itsmeumeshsoni@gmail.com>
2026-02-07 00:22:45 +0530

    Time: 0 ms (100.00%) | Memory: 117.8 MB (93.26%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -21,7 +21,7 @@ public:
         ListNode* h1 = head;
         ListNode* h2 = reverse(slow);
 
-        while(h1) {
+        while(h2->next) {
             if(h1->val != h2->val) return false;
 
             h1 = h1->next;
